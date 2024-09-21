1c4928690f5c38e2f93df8d1e7e59a910b63c627
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2024-09-21 12:23:10 +0530

    Added Keyboard input

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const num8 = document.querySelector("#num8");
 const num9 = document.querySelector("#num9");
 const num0 = document.querySelector("#num0");
 const dot = document.querySelector("#dot");
+const prev = document.querySelector("#ans");
 var input = document.querySelector(".display");
 var sum = document.querySelector("#sum");
 var minus = document.querySelector("#minus");
@@ -17,66 +18,166 @@ var divide = document.querySelector("#divide");
 var mod = document.querySelector("#mod");
 var clear = document.querySelector("#clear");
 var back = document.querySelector("#delete");
-var equals = document.querySelector("#eq"); 
+var equals = document.querySelector("#eq");
 
 let expression = "";
+let previous = "";
 function makenum(digit) {
-    expression += digit;
-    give();
-};
+  expression += digit;
+  give();
+}
 
 function clr() {
-    expression = ""; 
-    give();
+  expression = "";
+  give();
 }
 
 function del() {
-    expression = expression.substring(0, expression.length - 1);
-    give();
+  expression = expression.substring(0, expression.length - 1);
+  give();
 }
 
 function give() {
-    input.innerHTML = `${expression}`;
-};
+  input.innerHTML = `${expression}`;
+}
 
 function evaluateExpression() {
+  if (expression == "") {
+    input.innerHTML = "0";
+  } else {
     try {
-        const result = eval(expression); 
-        input.innerHTML = `${result}`;
-        expression = ""; 
+      const result = eval(expression);
+      input.innerHTML = `${result}`;
+      previous = result;
+      expression = "";
     } catch (error) {
-        input.innerHTML = "Error"; 
+      input.innerHTML = "Error";
     }
+  }
+}
+
+function storeAns() {
+  if (expression != "") {
+    evaluateExpression();
+  }
+  expression = previous;
 }
 
 function get() {
+  // for mobile
+  num1.addEventListener("click", () => {
+    makenum(1), vibrate(50);
+  });
+  num2.addEventListener("click", () => {
+    makenum(2), vibrate(50);
+  });
+  num3.addEventListener("click", () => {
+    makenum(3), vibrate(50);
+  });
+  num4.addEventListener("click", () => {
+    makenum(4), vibrate(50);
+  });
+  num5.addEventListener("click", () => {
+    makenum(5), vibrate(50);
+  });
+  num6.addEventListener("click", () => {
+    makenum(6), vibrate(50);
+  });
+  num7.addEventListener("click", () => {
+    makenum(7), vibrate(50);
+  });
+  num8.addEventListener("click", () => {
+    makenum(8), vibrate(50);
+  });
+  num9.addEventListener("click", () => {
+    makenum(9), vibrate(50);
+  });
+  num0.addEventListener("click", () => {
+    makenum(0), vibrate(50);
+  });
+  dot.addEventListener("click", () => {
+    makenum("."), vibrate(50);
+  });
+  sum.addEventListener("click", () => {
+    makenum("+"), vibrate(50);
+  });
+  minus.addEventListener("click", () => {
+    makenum("-"), vibrate(50);
+  });
+  multiply.addEventListener("click", () => {
+    makenum("*"), vibrate(50);
+  });
+  divide.addEventListener("click", () => {
+    makenum("/"), vibrate(50);
+  });
+  mod.addEventListener("click", () => {
+    makenum("%"), vibrate(50);
+  });
+  clear.addEventListener("click", () => {
+    clr(), vibrate(50);
+  });
+  back.addEventListener("click", () => {
+    del(), vibrate(50);
+  });
+  equals.addEventListener("click", () => {
+    evaluateExpression(), vibrate(100);
+  });
+  prev.addEventListener("click", () => {
+    storeAns(), vibrate(100);
+  });
 
-    num1.addEventListener("click", function () { makenum(1) });
-    num2.addEventListener("click", function () { makenum(2)  });
-    num3.addEventListener("click", function () { makenum(3)  });
-    num4.addEventListener("click", function () { makenum(4)  });
-    num5.addEventListener("click", function () { makenum(5)  });
-    num6.addEventListener("click", function () { makenum(6)  });
-    num7.addEventListener("click", function () { makenum(7)  });
-    num8.addEventListener("click", function () { makenum(8)  });
-    num9.addEventListener("click", function () { makenum(9)  });
-    num0.addEventListener("click", function () { makenum(0)  });
-    dot.addEventListener("click", function () { makenum('.')  });
-    sum.addEventListener("click", function () { makenum('+')  });
-    minus.addEventListener("click", function () { makenum('-')  });
-    multiply.addEventListener("click", function () { makenum('*')  });
-    divide.addEventListener("click", function () { makenum('/')  });
-    mod.addEventListener("click", function () { makenum('%')  });
-    clear.addEventListener("click", function () { clr() });
-    back.addEventListener("click", function () { del()  });
-    equals.addEventListener("click", function () { evaluateExpression()  }); 
-};
+  // Keyboard event listeners
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "1") {
+      makenum(1), vibrate(50);
+    } else if (e.key === "2") {
+      makenum(2), vibrate(50);
+    } else if (e.key === "3") {
+      makenum(3), vibrate(50);
+    } else if (e.key === "4") {
+      makenum(4), vibrate(50);
+    } else if (e.key === "5") {
+      makenum(5), vibrate(50);
+    } else if (e.key === "6") {
+      makenum(6), vibrate(50);
+    } else if (e.key === "7") {
+      makenum(7), vibrate(50);
+    } else if (e.key === "8") {
+      makenum(8), vibrate(50);
+    } else if (e.key === "9") {
+      makenum(9), vibrate(50);
+    } else if (e.key === "0") {
+      makenum(0), vibrate(50);
+    } else if (e.key === ".") {
+      makenum("."), vibrate(50);
+    } else if (e.key === "+") {
+      makenum("+"), vibrate(50);
+    } else if (e.keyCode === 107) {
+      makenum("+"), vibrate(50);
+    } else if (e.key === "-") {
+      makenum("-"), vibrate(50);
+    } else if (e.key === "*") {
+      makenum("*"), vibrate(50);
+    } else if (e.key === "/") {
+      makenum("/"), vibrate(50);
+    } else if (e.key === "%") {
+      makenum("%"), vibrate(50);
+    } else if (e.key === "Backspace") {
+      del(), vibrate(50);
+    } else if (e.key === "Enter") {
+      evaluateExpression(), vibrate(100);
+    } else if (e.key === "=") {
+      evaluateExpression(), vibrate(100);
+    } else if (e.key === "ArrowUp") {
+      storeAns(), vibrate(100);
+    } else if (e.key === "Delete") {
+      clr(), vibrate(50);
+    }
+  });
+}
 get();
 
-// Addition features 
-button=document.getElementsByTagName("input");
-button.addEventListener("click", vibrate());
-function vibrate() {
-    navigator.vibrate(50)  
-  }
-  
+// Addition features
+function vibrate(val) {
+  navigator.vibrate(val);
+}
